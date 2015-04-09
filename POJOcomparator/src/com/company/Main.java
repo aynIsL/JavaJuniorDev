1bdb5e5bb46e24526b30dc2d2ebb97ce21fec9e5
aynIsL <ivan.kyselov.ukr@gmail.com>
2015-04-09 16:07:46 +0300

    Builder

--- POJOcomparator/src/com/company/Main.java
+++ POJOcomparator/src/com/company/Main.java
@@ -2,26 +2,31 @@ package com.company;
 
 import com.person.Person;
 import com.person.PersonAgeComparator;
+import com.person.PersonNameComparator;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person person1 = new Person("Ivan", "Kyselov",30);
-        Person person2 = new Person("Tatiana", "Kyselova",30);
-        Person person3 = new Person("Olga", "Lysuk",57);
-        Person person4 = new Person("Yuriu", "Lisuk",30);
-        Person person5 = new Person("Lubov", "Kyselova",61);
-        Person person6 = new Person("Andrey", "Kyselov",60);
+        Person first = new Person.Builder().firstName("Ivan").lastName("Kiselev").age(30).build();
+        Person second = new Person.Builder().firstName("Tatiana").lastName("Kiseleva").age(29).build();
+        Person three = new Person.Builder().firstName("Misha").lastName("Kiselev").age(2).build();
 
-        Person[] family = {person1,person2,person3,person4,person5,person6};
-        System.out.println("Before Sorting ");
-        System.out.println(Arrays.toString(family));
-        System.out.println("-----------------------------------------");
-        System.out.println("After Sorting ");
-        Arrays.sort(family, new PersonAgeComparator());
-        System.out.println(Arrays.toString(family));
+        Person[] fam = new Person[]{first,second,three};
+        Arrays.sort(fam, new PersonAgeComparator());
+
+        System.out.println(Arrays.toString(fam));
+        Arrays.sort(fam, new PersonNameComparator());
+        System.out.println(Arrays.toString(fam));
+
+//        Person[] family = {person1,person2,person3,person4,person5,person6};
+//        System.out.println("Before Sorting ");
+//        System.out.println(Arrays.toString(family));
+//        System.out.println("-----------------------------------------");
+//        System.out.println("After Sorting ");
+//        Arrays.sort(family, new PersonAgeComparator());
+//        System.out.println(Arrays.toString(family));
 
 
     }
--- POJOcomparator/src/com/person/Person.java
+++ POJOcomparator/src/com/person/Person.java
@@ -1,47 +1,88 @@
 package com.person;
 
 public class Person {
-    private String firstName;
-    private String lastName;
-    private int age;
 
+    private final String firstName;
+    private final String lastName;
+    private final int age;
 
-
-    @Override
-    public String toString(){
-        return String.format("FirsName : " + firstName + ";" +
-                                " LastName : " + lastName + ";" +
-                                " Age :" + age + "\n");
-    }
-
-    public Person(String firstName, String lastName, int age) {
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    private Person(Builder builder){
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.age = builder.age;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (firstName != null ? !firstName.equals(person.firstName) : person.firstName != null) return false;
+        return !(lastName != null ? !lastName.equals(person.lastName) : person.lastName != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Name: " + firstName + " " + lastName + " ; Age: " + age );
+    }
+//    {
+//        return super.toString();
+//    }
+
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private int age;
+
+        public Builder(){}
+        public Builder (Person original){
+            this.firstName = original.firstName;
+            this.lastName = original.lastName;
+            this.age = original.age;
+        }
+
+            public Builder firstName(String firstName){
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName){
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder age(int age){
+            this.age = age;
+            return this;
+        }
+
+        public Person build(){
+            return new Person(this);
+        }
     }
 }
